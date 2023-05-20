91d9412139efcee6b486b9dd12feab2bf23e6831
615725 <615725@CS544-202305-23.cs.mum.edu>
2023-05-20 14:50:31 -0500

    added LineItem, Order, OrderStatus, Review

--- src/main/java/cs544/ea/OnlineRetailSystem/domain/Review.java
+++ src/main/java/cs544/ea/OnlineRetailSystem/domain/Review.java
@@ -1,4 +1,35 @@
 package cs544.ea.OnlineRetailSystem.domain;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
 public class Review {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "reviewId")
+	private int id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "numberOfStar")
+	private int numberOfStar;
+	
+	@Column(name = "reviewDate")
+	private LocalDateTime reviewDate;
+	
+	@ManyToOne
+	private Customer reviewer;
 }
