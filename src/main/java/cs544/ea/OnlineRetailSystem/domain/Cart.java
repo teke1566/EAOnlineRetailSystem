6d787861c0ae08bc41cd9f805a9e4ee615888ee9
YouKimChhay <youkim.chhay@miu.edu>
2023-05-20 15:58:42 -0500

    added cart and creditCard entities

--- src/main/java/cs544/ea/OnlineRetailSystem/domain/Cart.java
+++ src/main/java/cs544/ea/OnlineRetailSystem/domain/Cart.java
@@ -1,4 +1,31 @@
 package cs544.ea.OnlineRetailSystem.domain;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.OrderColumn;
+import lombok.Data;
+
+@Data
+@Entity
 public class Cart {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "cartId")
+	private int id;
+	
+	@OneToOne
+	private Customer customer;
+	
+	@OneToMany
+	@JoinColumn(name = "lineItemId")
+	@OrderColumn(name = "sequence")
+	private List<LineItem> lineItems;
 }
--- src/main/java/cs544/ea/OnlineRetailSystem/domain/CreditCard.java
+++ src/main/java/cs544/ea/OnlineRetailSystem/domain/CreditCard.java
@@ -1,4 +1,28 @@
 package cs544.ea.OnlineRetailSystem.domain;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class CreditCard {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "creditCardId")
+	private int id;
+	
+	@Column(name = "cardNumber")
+	private String cardNumber;
+	
+	@Column(name = "expirationDate")
+	private LocalDate expirationDate;
+	
+	@Column(name = "securityCode")
+	private String securityCode;
 }
