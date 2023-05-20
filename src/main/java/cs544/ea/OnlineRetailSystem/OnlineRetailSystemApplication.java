d0cac547e0a6bddd152f8bb08cbeec78ca897305
teke1566 <teke156605@gmail.com>
2023-05-20 13:43:50 -0500

    initial commit

---
+++ src/main/java/cs544/ea/OnlineRetailSystem/OnlineRetailSystemApplication.java
@@ -0,0 +1,13 @@
+package cs544.ea.OnlineRetailSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRetailSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRetailSystemApplication.class, args);
+	}
+
+}
