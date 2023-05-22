ad4d1b91c698f0501461501d5da13acec78d4ddb
NaodZere <naodmengis@gmail.com>
2023-05-22 16:52:34 -0500

    stash

--- src/main/java/cs544/ea/OnlineRetailSystem/repository/CartRepository.java
+++ src/main/java/cs544/ea/OnlineRetailSystem/repository/CartRepository.java
@@ -1,4 +1,7 @@
 package cs544.ea.OnlineRetailSystem.repository;
 
-public interface CartRepository {
+import cs544.ea.OnlineRetailSystem.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
