eb503925d2acb39b1cf10ed8c518d471f9f1c9a5
YouKimChhay <youkim.chhay@miu.edu>
2023-05-23 16:14:30 -0500

    fixed Order and Cart Services

--- src/main/java/cs544/ea/OnlineRetailSystem/service/CartService.java
+++ src/main/java/cs544/ea/OnlineRetailSystem/service/CartService.java
@@ -1,13 +1,14 @@
 package cs544.ea.OnlineRetailSystem.service;
 import cs544.ea.OnlineRetailSystem.domain.Cart;
+import cs544.ea.OnlineRetailSystem.domain.dto.request.ItemRequest;
+import cs544.ea.OnlineRetailSystem.domain.dto.response.OrderResponse;
 
 public interface CartService {
+	
     public Cart getCartByCustomerId(Long customerId);
-
-    public void addItemToCart();
-
-    public void removeItemFromCart();
-
-    public void clearCart();
+    public void addItemToCart(Long customerId, ItemRequest itemRequest) throws Exception;
+    public void removeItemFromCart(Long customerId, Long itemId);
+    public void clearCart(Long customerId);
+    public OrderResponse checkoutCart(Long customerId);
 
 }
