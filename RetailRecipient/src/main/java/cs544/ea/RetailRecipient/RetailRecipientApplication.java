e724bd6a253f8eae65299f5129568fcea24b6003
teke1566 <teke156605@gmail.com>
2023-05-23 03:45:12 -0500

    initial

---
+++ RetailRecipient/src/main/java/cs544/ea/RetailRecipient/RetailRecipientApplication.java
@@ -0,0 +1,13 @@
+package cs544.ea.RetailRecipient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailRecipientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailRecipientApplication.class, args);
+	}
+
+}
---
+++ RetailRecipient/src/test/java/cs544/ea/RetailRecipient/RetailRecipientApplicationTests.java
@@ -0,0 +1,13 @@
+package cs544.ea.RetailRecipient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailRecipientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
